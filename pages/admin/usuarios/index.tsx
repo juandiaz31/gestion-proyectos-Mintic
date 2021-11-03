546fafb726eb01e0fb58607ae027a45eb6698b2e
Juan <jsdiazg1@gmail.com>
2021-11-02 22:13:26 -0500

    agregue tailwind y rutas ejemplo

---
+++ pages/admin/usuarios/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>Hola desde admin usuarios</div>;
+};
+
+export default index;
---
+++ pages/sebastian.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sebastian = () => {
+  return <div>hola desde sebastian</div>;
+};
+
+export default sebastian;
