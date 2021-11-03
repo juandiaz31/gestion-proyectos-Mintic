b7f8dafb13298dd9b08332a8e7745cf4e06c5b99
Juan <jsdiazg1@gmail.com>
2021-11-02 22:46:38 -0500

    se añade link para ir a admin/usuarios

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return <div className="bg-green-500">hola mundo, soy next js</div>;
+  return (
+    <div>
+      <div className="bg-green-500">hola mundo, soy next js</div>
+      <Link href="/admin/usuarios">
+      <a>Ir a admin/usuarios</a>
+      </Link>
+      <Link href="/sebastian">
+      <a>Ir a sebastian</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
