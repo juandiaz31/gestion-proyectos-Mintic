b89b28ece1b9b96e0095276b3f929a5a4658e21d
Juan <jsdiazg1@gmail.com>
2021-11-02 22:24:51 -0500

    se añade la funcion de ir al home desde admin/usuarios

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const index = () => {
-  return <div>Hola desde admin usuarios</div>;
+  return (
+    <div>
+      <div>Hola desde admin usuarios</div>
+      <Link href="/">
+        <a>Ir a la pagina de inicion</a>
+      </Link>
+    </div>
+  );
 };
 
 export default index;
