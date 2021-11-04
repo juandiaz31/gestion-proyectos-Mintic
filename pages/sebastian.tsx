df93d64cf43d0ec60e21e0863fdd90850e2e76c5
Juan <jsdiazg1@gmail.com>
2021-11-03 21:27:30 -0500

    para volver a home desde sebastian

--- pages/sebastian.tsx
+++ pages/sebastian.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const sebastian = () => {
-  return <div>hola desde sebastian</div>;
+  return (
+    <div>
+      <div>hola desde sebastian</div>
+      <Link href="/">
+        <a>Ir a la pagina de inicio</a>
+      </Link>
+    </div>
+  );
 };
 
 export default sebastian;
